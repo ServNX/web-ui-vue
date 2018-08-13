1f089d14985fea002dd17e780bdb4f007277de7d
poppabear8883 <servnx@gmail.com>
2018-08-13 16:14:26 -0400

    Init Commit

---
+++ src/components/MainCarousel.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-carousel>
+    <v-carousel-item :src="items[0].src">
+      <v-jumbotron>
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex>
+              <h3 class="display-3">Welcome to the site</h3>
+
+              <span class="subheading">
+                Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae
+                id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare
+                assueverit ex eum, ne nam essent vocent admodum.
+              </span>
+
+              <v-divider class="my-3"></v-divider>
+
+              <div class="title mb-3">Check out our newest features!</div>
+
+              <v-btn
+                class="mx-0"
+                color="primary"
+                large
+              >
+                See more
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+    </v-carousel-item>
+
+    <v-carousel-item :src="items[1].src">
+      <v-jumbotron>
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex>
+              <h3 class="display-3">YAY</h3>
+
+              <span class="subheading">
+                Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae
+                id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare
+                assueverit ex eum, ne nam essent vocent admodum.
+              </span>
+
+              <v-btn
+                class="mx-0"
+                color="primary"
+                large
+              >
+                See more
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+    </v-carousel-item>
+
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    name: 'MainCarousel',
+    data() {
+      return {
+        items: [
+          {
+            id: 1,
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            id: 2,
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            id: 3,
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            id: 4,
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      };
+    },
+  };
+</script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-header">
+    <v-toolbar>
+      <v-toolbar-title>ServNX</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+
+        <v-btn @click="route('/')" flat>
+          Home
+        </v-btn>
+
+        <v-btn @click="route('/projects')" flat>
+          Projects
+        </v-btn>
+
+        <v-btn @click="route('/products')" flat>
+          Products
+        </v-btn>
+
+        <v-btn @click="route('/services')" flat>
+          Services
+        </v-btn>
+
+        <v-btn @click="route('/about')" flat>
+          About
+        </v-btn>
+
+        <v-btn @click="route('/login')" flat>
+          Login
+        </v-btn>
+
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainHeader',
+    methods: {
+      route($page) {
+        this.$router.push($page);
+      },
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
