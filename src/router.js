15f1f3236d848019380d109f174b6d49b838c61a
poppabear8883 <servnx@gmail.com>
2018-08-16 16:21:11 -0400

    chore: Add views and update router.js

--- src/router.js
+++ src/router.js
@@ -14,12 +14,29 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/projects',
+      name: 'projects',
+      component: () => import(/* webpackChunkName: "projects" */ './views/Projects.vue'),
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import(/* webpackChunkName: "products" */ './views/Products.vue'),
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import(/* webpackChunkName: "services" */ './views/Services.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
     },
   ],
 });
---
+++ src/views/Projects.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is the Projects page</h1>
+  </div>
+</template>
