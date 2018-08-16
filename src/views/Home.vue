e446cb568b1b24ec0397b02564e4fa6a3d25703c
poppabear8883 <servnx@gmail.com>
2018-08-16 16:20:12 -0400

    ui: Add temporary content to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,97 @@
 <template>
-  <div class="home">
+  <div class="home" style="min-height: 900px">
     <main-carousel></main-carousel>
 
+    <v-card class="flex" flat tile>
+      <v-card-title class="white" light>
+        <strong class="subheading">Get involve with our community!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn outline class="mx-0 blue--text text--lighten-2" large>
+          <font-awesome-icon :icon="['fab', 'discord']" style="font-size: 24px"/>&nbsp; Join Our
+          Discord Now!
+        </v-btn>
+
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center white--text">
+
+      </v-card-actions>
+    </v-card>
+
+    <v-layout
+      column
+      wrap
+      class="my-5"
+      align-center
+    >
+      <v-flex xs12 sm4 class="my-3">
+        <div class="text-xs-center">
+          <h2 class="headline">The best way to start developing</h2>
+          <span class="subheading">
+                Cras facilisis mi vitae nunc
+              </span>
+        </div>
+      </v-flex>
+      <v-flex xs12>
+        <v-container grid-list-xl>
+          <v-layout row wrap align-center>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline text-xs-center">Material Design</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                  turpis egestas.
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                  Suspendisse potenti.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Fast development</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                  turpis egestas.
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                  Suspendisse potenti.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 md4>
+              <v-card class="elevation-0 transparent">
+                <v-card-text class="text-xs-center">
+                  <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                </v-card-text>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline text-xs-center">Completely Open Sourced</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                  turpis egestas.
+                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
+                  Suspendisse potenti.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
