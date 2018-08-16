4f58f82086e50fa843726e242b32f7c3fe1cc91b
poppabear8883 <servnx@gmail.com>
2018-08-16 16:21:58 -0400

    ui: Update footer

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
 
       <router-view/>
 
-      <v-footer fixed light height="auto">
+      <v-footer absolue light height="auto">
         <v-card class="flex" flat tile>
-          <v-card-title class="white">
+          <v-card-title class="white" light>
             <strong class="subheading">Get connected with us on social networks!</strong>
 
             <v-spacer></v-spacer>
 
             <v-btn v-for="icon in icons" :key="icon" class="mx-3" light icon>
-              <font-awesome-icon :icon="['fab', icon]" style="font-size: 24px"/>
+              <font-awesome-icon class="footer-social-icon" :icon="['fab', icon]" />
             </v-btn>
 
           </v-card-title>
