14f030ec8c74227948fde71d97867c616dc9ef9a
poppabear8883 <servnx@gmail.com>
2018-08-26 22:01:12 -0400

    ui(View): Create dashboard view

---
+++ src/views/Admin/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="dashboard">
+        <h1>Welcome to the dashboard !!</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'nx-admin-dashboard',
+    beforeMount() {
+      this.$store.commit('SET_LAYOUT', 'app-layout');
+    },
+  };
+</script>
