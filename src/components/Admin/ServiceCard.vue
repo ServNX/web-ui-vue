5ed79fb67c8b4cce126a01fff10bcbe351e57c39
poppabear8883 <servnx@gmail.com>
2018-08-27 21:54:05 -0400

    wip(Services): Add services view with a service card component

---
+++ src/components/Admin/ServiceCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+            <v-card>
+                <v-card-media
+                        :src="src"
+                        height="200px"
+                >
+                </v-card-media>
+
+                <v-card-title primary-title v-if="title">
+                    <div>
+                        <div class="headline">{{ title }}</div>
+                    </div>
+                </v-card-title>
+
+                <v-card-actions>
+                    <v-btn :href="link" outline color="accent" :disabled="disabled">Link</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn icon @click="show = !show" v-if="description">
+                        <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                    </v-btn>
+                </v-card-actions>
+
+                <v-slide-y-transition>
+                    <v-card-text v-show="show">
+                        {{ description }}
+                    </v-card-text>
+                </v-slide-y-transition>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    name: 'nx-service-card',
+    props: {
+      disabled: { type: Boolean, default: false },
+      title: { type: String, default: null },
+      description: { type: String, default: null },
+      src: { type: String, required: true },
+      link: { type: String, required: true },
+    },
+    data() {
+      return {
+        show: false,
+      };
+    },
+  };
+</script>
---
+++ src/views/Admin/Services.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+            <v-flex xs6>
+                <nx-service-card title="Github"
+                                 :link="`${link}/auth/github`"
+                                 src="https://cdn-images-1.medium.com/max/1200/1*wzEf4qVNT7mUeMIeQWTJbg.jpeg"/>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+  import NxServiceCard from '../../components/Admin/ServiceCard';
+
+  export default {
+    name: 'nx-admin-dashboard',
+    components: {
+      NxServiceCard,
+    },
+    computed: {
+      link() {
+        return process.env.VUE_APP_API_ENDPOINT;
+      },
+    },
+  };
+</script>
