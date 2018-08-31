fa57f1fa69ebc7e011245f7667e4315a1c035ff6
poppabear8883 <servnx@gmail.com>
2018-08-30 22:06:04 -0400

    ui(Repositories): Add data iterator component for listing repositories

--- src/views/Admin/Repositories.vue
+++ src/views/Admin/Repositories.vue
@@ -1,7 +1,59 @@
 <template>
-  <section>
-    <h1>Repositories for {{ service }}</h1>
-  </section>
+  <v-container fluid grid-list-md>
+    <v-data-iterator
+        :items="repositories"
+        :rows-per-page-items="rowsPerPageItems"
+        :pagination.sync="pagination"
+        content-tag="v-layout"
+        row
+        wrap
+    >
+
+      <v-flex slot="item"
+              slot-scope="props"
+              xs12
+              sm6
+              md4
+              lg3
+      >
+        <v-card>
+          <v-card-title class="subheading font-weight-bold">{{ props.item.name }}</v-card-title>
+
+          <v-divider></v-divider>
+
+          <v-list dense>
+            <v-list-tile>
+              <v-list-tile-content>Open Issues:</v-list-tile-content>
+              <v-list-tile-content class="align-end">
+                {{ props.item.open_issues }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-content>Forks:</v-list-tile-content>
+              <v-list-tile-content class="align-end">
+                {{ props.item.forks_count }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-content>Stars:</v-list-tile-content>
+              <v-list-tile-content class="align-end">
+                {{ props.item.stargazers_count }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-content>Watchers:</v-list-tile-content>
+              <v-list-tile-content class="align-end">
+                {{ props.item.watchers_count }}
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-data-iterator>
+  </v-container>
 </template>
 
 <script>
@@ -12,6 +64,10 @@
     },
     data() {
       return {
+        rowsPerPageItems: [4, 8, 12],
+        pagination: {
+          rowsPerPage: 8,
+        },
         repositories: [],
       };
     },
@@ -28,7 +84,9 @@
     },
     mounted() {
       this.axios.get(this.repositories_endpoint)
-        .then(response => console.log(response))
+        .then(response => {
+          this.repositories = response.data;
+        })
         .catch(error => console.log(error));
     },
   };
